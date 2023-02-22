7ea89daaa4f03d139a0b3c21cb5c3a0f31f1cafd
logan hillyer <loganthomashillyer@gmail.com>
2023-02-22 18:10:18 +0530

    google sheets database

--- app.js
+++ app.js
@@ -1,39 +1,57 @@
 
-key = '3CD5N2DHC08I';
-fetch('https://api.timezonedb.com/v2.1/get-time-zone?key='+key+'&format=json&by=zone&zone=Asia/Kolkata')
-.then(response => response.json()) // Parse the response as JSON
-  .then(data => {
-    // Do something with the data
-    console.log(data);
-    const offset  = data.gmtOffset/(60*60); // UTC offset in hours
+const sheetId = '1JzDy_C4Tss6PGfVfVBomcd42knABlTHPk7Zl_jrvPws';
+const sheetName = 'Sheet1';
 
-    const difference = document.getElementById('difference');
+const url = `https://opensheet.elk.sh/${sheetId}/${sheetName}`;
 
-    // set the content of the div to the field value
-    difference.textContent = ''+(offset+8)+' hours ahead of Santa Barbara time';
+console.log(url)
 
-    const now = new Date();
+fetch(url)
+    .then(response => response.json()) // Parse the response as JSON
+    .then(data => {
+        const latitude = data[0].latitude;
+        const longitude = data[0].longitude;
+        console.log(latitude)
+        console.log(longitude)
 
-    // Get the current time in Los Angeles
-    const losAngelesTime = now.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
-    console.log(`Los Angeles time: ${losAngelesTime}`);
+        key = '3CD5N2DHC08I';
+        fetch('https://api.timezonedb.com/v2.1/get-time-zone?key=' + key + '&format=json&by=position&lat=' + latitude + '&lng=' + longitude)
+            .then(response => response.json()) // Parse the response as JSON
+            .then(data => {
+                // Do something with the data
+                console.log(data);
+                const offset = data.gmtOffset / (60 * 60); // UTC offset in hours
 
-    // Get the current time in Paris
-    const parisTime = now.toLocaleString('en-US', { timeZone: 'Europe/Paris' });
-    console.log(`Paris time: ${parisTime}`);
+                const difference = document.getElementById('difference');
+                const differenceb = document.getElementById('differenceb');
 
-    // Create a table showing how the times in Los Angeles and Paris correspond
-    console.table([
-      { 'Time zone': 'Los Angeles', 'Time': losAngelesTime, 'Corresponding time in Paris': parisTime },
-      { 'Time zone': 'Paris', 'Time': parisTime, 'Corresponding time in Los Angeles': losAngelesTime }
-    ]);
+                // set the content of the div to the field value
+                difference.textContent = `Logan is ${offset + 8} hours ahead of Santa Barbara time`;
+
+                differenceb.textContent = `Logan is ${offset + 5} hours ahead of D.C.  time`;
 
 
-    const b = document.getElementById('b');
-    b.textContent = 55;
-  })
-  .catch(error => {
-    // Handle any errors
-    console.error(error);
-    console.error('there is an error happening Logan!');
-  });
+                const now = new Date();
+                const future = new Date(now.getTime() + (offset + 0) * (60 * 60));
+
+                const time = future.toLocaleTimeString([], { hour: 'numeric', minute: 'numeric' });
+
+                console.log(`The time is: ${time}`);
+                const loganTime = document.getElementById('loganTime');
+                loganTime.textContent = `it is ${time} where he is`;
+
+            })
+            .catch(error => {
+                // Handle any errors
+                console.error(error);
+                console.error('there is an error happening Logan!');
+            });
+
+    })
+    .catch(error => {
+        // Handle any errors
+        console.error(error);
+        console.error('there is an error happening Logan!');
+    });
+
+
